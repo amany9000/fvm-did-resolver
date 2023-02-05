f72309d6829fd9b63a418f4a3963069915720681
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2023-02-05 22:46:01 +0530

    remove log4js

--- src/fvm-did-resolver.ts
+++ src/fvm-did-resolver.ts
@@ -1,12 +1,13 @@
-import * as log4js from "log4js";
-import * as networkConfiguration from "./configuration.json";
-import { ethers } from "ethers";
-import { DIDDocument, DIDResolutionResult, DIDResolver, ParsedDID, Resolver } from 'did-resolver';
-const DidRegistryContract = require("fvm-did-registry-contract");
-
-
-const logger = log4js.getLogger();
-logger.level = `debug`;
+import * as networkConfiguration from './configuration.json';
+import { ethers } from 'ethers';
+import {
+    DIDDocument,
+    DIDResolutionResult,
+    DIDResolver,
+    ParsedDID,
+    Resolver,
+} from 'did-resolver';
+const DidRegistryContract = require('fvm-did-registry-contract');
 
 /**
  * Resolves DID Document.
@@ -14,87 +15,83 @@ logger.level = `debug`;
  * @returns Return DID Document on chain.
  */
 
-export function getResolver (): Record<string, DIDResolver> {
-      async function resolve(did: string, parsed?: ParsedDID): Promise<DIDResolutionResult> {
+export function getResolver(): Record<string, DIDResolver> {
+    async function resolve(
+        did: string,
+        parsed?: ParsedDID
+    ): Promise<DIDResolutionResult> {
+        const didDocumentMetadata = {};
+        try {
+            let errorMessage: string;
+            let url: string;
+            let contractAddress: string;
+            const didWithTestnet: string = await splitfvmDid(did);
 
-            const didDocumentMetadata = {}
-            try {
-                  let errorMessage: string;
-                  let url: string;
-                  let contractAddress: string;
-                  const didWithTestnet: string = await splitfvmDid(did);
-
-                  if (
-                        (did &&
-                              didWithTestnet === "testnet" &&
-                              did.match(/^did:fvm:testnet:0x[0-9a-fA-F]{40}$/)) ||
-                        (did && did.match(/^did:fvm:0x[0-9a-fA-F]{40}$/))
-                  ) {
-                        if (
-                              (didWithTestnet === "testnet" &&
-                                    did.match(/^did:fvm:testnet:\w{0,42}$/)) ||
-                              did.match(/^did:fvm:\w{0,42}$/)
-                        ) {
-                              if (did && didWithTestnet === "testnet") {
-                                    url = `${networkConfiguration[0].testnet?.URL}`;
-                                    contractAddress = `${networkConfiguration[0].testnet?.CONTRACT_ADDRESS}`;
-                              } else {
-                                    errorMessage = `Wrong network enter!`;
-                                    throw new Error(errorMessage);
-                              }
-
-                              const provider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider(
-                                    url
-                              );
-                              const registry: ethers.Contract = new ethers.Contract(
-                                    contractAddress,
-                                    DidRegistryContract.abi,
-                                    provider
-                              );
-
-                              const didAddress: string =
-                                    didWithTestnet === "testnet" ? did.split(":")[3] : didWithTestnet;
-
-                              // Calling smart contract with getting DID Document
-                              let didDocument: any = await registry.functions
-                                    .getDIDDoc(didAddress)
-                                    .then((resValue: any) => {
-                                          return resValue;
-                                    });
-
-                              logger.debug(
-                                    `[resolveDID] readDIDDoc - ${JSON.stringify(didDocument)} \n\n\n`
-                              );
-
-                              if (didDocument && !didDocument.includes("")) {
-                                    return{
-                                          didDocument,
-                                          didDocumentMetadata,
-                                          didResolutionMetadata: {contentType: 'application/did+ld+json'}
-                                    }
-                              } else {
-                                    errorMessage = `The DID document for the given DID was not found!`;
-                                    logger.error(errorMessage);
-                                    throw new Error(errorMessage);
-                              }
-                        } else {
-                              errorMessage = `Invalid address has been entered!`;
-                              logger.error(errorMessage);
-                              throw new Error(errorMessage);
-                        }
-                  } else {
-                        errorMessage = `Invalid DID has been entered!`;
-                        logger.error(errorMessage);
+            if (
+                (did &&
+                    didWithTestnet === 'testnet' &&
+                    did.match(/^did:fvm:testnet:0x[0-9a-fA-F]{40}$/)) ||
+                (did && did.match(/^did:fvm:0x[0-9a-fA-F]{40}$/))
+            ) {
+                if (
+                    (didWithTestnet === 'testnet' &&
+                        did.match(/^did:fvm:testnet:\w{0,42}$/)) ||
+                    did.match(/^did:fvm:\w{0,42}$/)
+                ) {
+                    if (did && didWithTestnet === 'testnet') {
+                        url = `${networkConfiguration[0].testnet?.URL}`;
+                        contractAddress = `${networkConfiguration[0].testnet?.CONTRACT_ADDRESS}`;
+                    } else {
+                        errorMessage = `Wrong network enter!`;
                         throw new Error(errorMessage);
-                  }
-            } catch (error) {
-                  logger.error(`Error occurred in resolve function ${error}`);
-                  throw error;
-            }
-      }
-      return { fvm: resolve };
-}
+                    }
 
+                    const provider: ethers.providers.JsonRpcProvider =
+                        new ethers.providers.JsonRpcProvider(url);
+                    const registry: ethers.Contract = new ethers.Contract(
+                        contractAddress,
+                        DidRegistryContract.abi,
+                        provider
+                    );
+
+                    const didAddress: string =
+                        didWithTestnet === 'testnet'
+                            ? did.split(':')[3]
+                            : didWithTestnet;
+
+                    // Calling smart contract with getting DID Document
+                    let didDocument: any = await registry.functions
+                        .getDIDDoc(didAddress)
+                        .then((resValue: any) => {
+                            return resValue;
+                        });
+
+                    if (didDocument && !didDocument.includes('')) {
+                        return {
+                            didDocument,
+                            didDocumentMetadata,
+                            didResolutionMetadata: {
+                                contentType: 'application/did+ld+json',
+                            },
+                        };
+                    } else {
+                        errorMessage = `The DID document for the given DID was not found!`;
+                        throw new Error(errorMessage);
+                    }
+                } else {
+                    errorMessage = `Invalid address has been entered!`;
+                    throw new Error(errorMessage);
+                }
+            } else {
+                errorMessage = `Invalid DID has been entered!`;
+                throw new Error(errorMessage);
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+    return { fvm: resolve };
+}
 
 /**
  * Split fvm DID.
@@ -102,6 +99,6 @@ export function getResolver (): Record<string, DIDResolver> {
  * @returns Returns Split data value to fvm DID.
  */
 async function splitfvmDid(did: string): Promise<string> {
-      const splitDidValue: string = did.split(":")[2];
-      return splitDidValue;
+    const splitDidValue: string = did.split(':')[2];
+    return splitDidValue;
 }
